0c63d3a6f7b5e634376eee7cc45c57bb0db148a6
Anucha Prapphrom <anucha.21681@hotmail.com>
2020-02-22 16:48:27 +0700

    feat(common): Add swagger API documents

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as YAML from 'yamljs';
+import * as swaggerUi from 'swagger-ui-express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  const swaggerDocument = YAML.load('./swagger.yaml');
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
   await app.listen(process.env.SERVER_PORT);
 }
 bootstrap();
